4a2e960caa062c07459d483349671e3a2a0a4ac0
ValR08813 <valentin.deschins@gmail.com>
2022-05-14 13:36:24 +0200

    premiere route okay

--- app/router.js
+++ app/router.js
@@ -1,4 +1,13 @@
 const {Router} = require ('express');
+const userController = require('./controllers/userController');
+const msgController = require('./controllers/msgController');
+const authentification = require('./middlewares/jwtMiddleware');
+
+
 const router = Router();
+router.get('/users', userController.findAll);
+
+router.get('/user/:id', userController.getUserInfos);
+
 
 module.exports = router; 
